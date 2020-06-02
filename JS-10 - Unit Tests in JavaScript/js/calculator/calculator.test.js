8ba12dc4370fcb65e0187648480bd43dbd253d0b
devashish-acharya <thedev96@gmail.com>
2020-06-02 14:56:37 +0100

    test

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -15,3 +15,19 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divide 20 / 5 to equal 4', () => {
+  expect(calculator.divide(20, 5)).toBe(4);
+});
+
+test('divide 6 / 3 to equal 2', () => {
+  expect(calculator.divide(6, 3)).toBe(2);
+});
+
+test('multiply 5 * 10 to equal 50', () => {
+  expect(calculator.multiply(5, 10)).toBe(50);
+});
+
+test('multiply 7 * 3 to equal 21', () => {
+  expect(calculator.multiply(7, 3)).toBe(21);
+});
--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -16,4 +16,31 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+  test('user is not in list', () => {
+    let user = new User(1234,"dev", "dev@generation.org");  
+    expect(userController.users).not.toContain(user);
+  });
+  
+  test('user is not in list with remove', () => {
+    let user = new User(1234,"dev", "dev@generation.org"); 
+    userController.remove(user);
+    expect(userController.users).not.toContain(user);
+  });
 
+  test('find by email', () => {
+    let  user = new User(1234,"Santiago", "santiago@generation.org");
+    let email = "santiago@generation.org";
+    expect(userController.findByEmail(email)).toEqual(user);
+  });
+
+  test('find by email not there', () => {
+    let user = new User(1234, "Santiago", "santiago@generation.org");
+    let email = "thedev96@gmail.com";
+    expect(userController.findByEmail(email)).not.toEqual(user);
+  });
+
+  test('find by id', () => {
+    let user = new User(1234, "Santiago", "santiago@generation.org");
+    let id = 1234;
+    expect(userController.findById(id)).toEqual(users);
+  });
